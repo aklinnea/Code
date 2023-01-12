ffd96ab63e671d89f1367eea39b84c31c0bfe062
aklinnea <aklinnea89gmail.com>
2023-01-12 10:44:33 +0100

    testing js

---
+++ Javascript/js-basics/index.js
@@ -0,0 +1,33 @@
+        // This is my first JS-code!             
+
+        function multiply(a, b) {
+            return a * b; 
+        }
+
+        function divide(a, b) {
+            return a / b;
+        }
+
+        console.log(multiply(4, 8));
+        console.log(multiply(6, 9));
+
+        console.log(divide(90, 7));
+        console.log(137, 23);
+
+        function plotValue(myName) {
+            console.log(myName);
+        }
+
+        plotValue('Linnea');
+
+        ///gjennomsnitt alder
+        function averageAge(a, b, c) {
+            const totalAge = a + b + c;
+            console.log(totalAge / 3);
+        }
+
+        averageAge(26, 65, 42);
+
+
+
+        
